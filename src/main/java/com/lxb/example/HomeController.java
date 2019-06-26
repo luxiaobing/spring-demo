b51908cf0657cba0a2ddf3157c9754feb8487142
luxiaobing <luxiaobing2014@163.com>
2019-06-26 22:02:08 +0800

    增加excel导出功能

--- src/main/java/com/lxb/example/HomeController.java
+++ src/main/java/com/lxb/example/HomeController.java
@@ -1,16 +1,26 @@
 package com.lxb.example;
 
 import com.alibaba.fastjson.JSON;
+import com.lxb.excel.ExportExcelUtils;
 import com.lxb.model.UserInfoDo;
 import com.lxb.service.IUserInfoService;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
+
 import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.http.HttpServletResponse;
+
 
 /**
  * ${DESCRIPTION}
@@ -109,5 +119,58 @@ public class HomeController {
         log.info("end saveTest");
         //返回一个index.jsp这个视图
         return "";
+    }
+
+    @RequestMapping("/downloadExcel")
+    public  String downloadExcel(HttpServletRequest request, final HttpServletResponse response){
+        String fileName = "test.xls";
+        try {
+            OutputStream out = new FileOutputStream(fileName);
+            List<List<String>> data1 = new ArrayList<List<String>>();//sheet1 data
+            for (int i = 1; i < 5; i++) {
+                List rowData = new ArrayList();
+                rowData.add(String.valueOf(i));
+                rowData.add("data1");
+                data1.add(rowData);
+            }
+            List<List<String>> data2 = new ArrayList<List<String>>();//sheet2 data
+            for (int i = 1; i < 5; i++) {
+                List rowData = new ArrayList();
+                rowData.add(String.valueOf(i));
+                rowData.add("data2");
+                data2.add(rowData);
+            }
+            List<List<String>> data3 = new ArrayList<List<String>>();//sheet3 data
+            for (int i = 1; i < 5; i++) {
+                List rowData = new ArrayList();
+                rowData.add(String.valueOf(i));
+                rowData.add("data3");
+                data3.add(rowData);
+            }
+            String[] firstHeaders1 = {"X月X日-X日xxx（单位名称）酒店账单明细表"};
+            String[] secHeaders1 = {"X月X日-X日xxx（单位名称）酒店账单明细表"};
+            String[] thirdHeaders1 = {"X月X日-X日xxx（单位名称）酒店账单明细表"};
+
+            String[] firstHeaders2 = { "航班号","舱位","乘机日期/时间","票价","改期费","基建费","燃油费","票总价","保险","退票费","服务费","客人应收","票状态"};
+            String[] secHeaders2 = { "TR单号","订单号","出票日期","退票日期","部门","姓名","行程","车次","席别","乘车日期/时间","票价","改签差价","改签手续费","票总价","退票费","保险","系统使用费","客人应收"};
+            String[] thirdHeaders2 = { "TR单号","订单号","预订日期","部门","姓名","酒店名称","房型","入住日期","离店日期","间夜","平均单价","总价","服务费","客人应收","支付类型"};
+
+            ExportExcelUtils eeu = new ExportExcelUtils();
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            eeu.exportExcel(workbook, 0, "机票", firstHeaders1, firstHeaders2, data1, out);
+            eeu.exportExcel(workbook, 1, "火车票", secHeaders1, secHeaders2, data2, out);
+            eeu.exportExcel(workbook, 2, "酒店", thirdHeaders1, thirdHeaders2, data3, out);
+
+            //原理就是将所有的数据一起写入，然后再关闭输入流。
+            workbook.write(out);
+
+            /*final StringBuffer filePath = new StringBuffer();
+            filePath.append(tmpOrderFolder).append(String.format("sheet_%d.xls", System.currentTimeMillis()));*/
+            eeu.download(fileName, response);
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return  "";
     }
 }
---
+++ src/main/java/com/lxb/excel/ExportExcelUtils.java
@@ -0,0 +1,206 @@
+package com.lxb.excel;
+
+import java.io.OutputStream;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFRichTextString;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.hssf.util.CellRangeAddress;
+
+import javax.servlet.http.HttpServletResponse;
+
+
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author Xiaobing.Lu
+ * @create 2019-06-25 22:12
+ **/
+public class ExportExcelUtils {
+    public void exportExcel(HSSFWorkbook workbook, int sheetNum,
+                            String sheetTitle, String[] headers, List<List<String>> result,
+                            OutputStream out) throws Exception {
+        // 生成一个表格
+        HSSFSheet sheet = workbook.createSheet();
+        workbook.setSheetName(sheetNum, sheetTitle);
+        // 设置表格默认列宽度为20个字节
+        sheet.setDefaultColumnWidth((short) 20);
+        // 生成一个样式
+        HSSFCellStyle style = workbook.createCellStyle();
+        // 设置这些样式
+        style.setFillForegroundColor(HSSFColor.PALE_BLUE.index);
+        style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+        style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
+        style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
+        style.setBorderRight(HSSFCellStyle.BORDER_THIN);
+        style.setBorderTop(HSSFCellStyle.BORDER_THIN);
+        style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+        // 生成一个字体
+        HSSFFont font = workbook.createFont();
+        font.setColor(HSSFColor.BLACK.index);
+        font.setFontHeightInPoints((short) 12);
+        font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
+        // 把字体应用到当前的样式
+        style.setFont(font);
+
+        // 指定当单元格内容显示不下时自动换行
+        style.setWrapText(true);
+
+        // 产生表格标题行
+        HSSFRow row = sheet.createRow(0);
+        for (int i = 0; i < headers.length; i++) {
+            HSSFCell cell = row.createCell((short) i);
+
+            cell.setCellStyle(style);
+            HSSFRichTextString text = new HSSFRichTextString(headers[i]);
+            cell.setCellValue(text.toString());
+        }
+        // 遍历集合数据，产生数据行
+        if (result != null) {
+            int index = 1;
+            for (List<String> m : result) {
+                row = sheet.createRow(index);
+                int cellIndex = 0;
+                for (String str : m) {
+                    HSSFCell cell = row.createCell((short) cellIndex);
+                    cell.setCellValue(str.toString());
+                    cellIndex++;
+                }
+                index++;
+            }
+        }
+    }
+
+
+    public void exportExcel(HSSFWorkbook workbook, int sheetNum,
+                            String sheetTitle, String[] fistHeaders, String[] secondHeaders, List<List<String>> result,
+                            OutputStream out) throws Exception {
+        // 生成一个表格
+        HSSFSheet sheet = workbook.createSheet();
+        workbook.setSheetName(sheetNum, sheetTitle);
+        // 设置表格默认列宽度为20个字节
+        sheet.setDefaultColumnWidth((short) 20);
+        // 生成一个样式
+        HSSFCellStyle style = workbook.createCellStyle();
+        // 设置这些样式
+        style.setFillForegroundColor(HSSFColor.PALE_BLUE.index);
+        style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+        style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
+        style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
+        style.setBorderRight(HSSFCellStyle.BORDER_THIN);
+        style.setBorderTop(HSSFCellStyle.BORDER_THIN);
+        style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+        // 生成一个字体
+        HSSFFont font = workbook.createFont();
+        font.setColor(HSSFColor.BLACK.index);
+        font.setFontHeightInPoints((short) 12);
+        font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
+        // 把字体应用到当前的样式
+        style.setFont(font);
+
+        // 指定当单元格内容显示不下时自动换行
+        style.setWrapText(true);
+
+        // 产生表格标题行
+        HSSFRow row = sheet.createRow(0);
+        for (int i = 0; i < fistHeaders.length; i++) {
+            HSSFCell cell = row.createCell((short) i);
+
+            cell.setCellStyle(style);
+            HSSFRichTextString text = new HSSFRichTextString(fistHeaders[i]);
+            cell.setCellValue(text.toString());
+        }
+
+        sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, secondHeaders.length - 1));
+
+        row = sheet.createRow(1);
+        for (int i = 0; i < secondHeaders.length; i++) {
+            HSSFCell cell = row.createCell((short) i);
+
+            cell.setCellStyle(style);
+            HSSFRichTextString text = new HSSFRichTextString(secondHeaders[i]);
+            cell.setCellValue(text.toString());
+        }
+
+        /*for (int i = 0; i < headers.length; i++) {
+            HSSFCell cell = row.createCell((short) i);
+            row = sheet.createRow(i);
+            row.setHeight((short) 700);
+            for (int j = 0; j < headers[i].length; j++) {
+                cell = row.createCell(j);
+                cell.setCellStyle(style);
+                cell.setCellValue(headers[i][j]);
+            }
+        }*/
+
+       /* for (int i = 0; i < headers.length; i++) {
+            HSSFCell cell = row.createCell((short) i);
+
+            cell.setCellStyle(style);
+            HSSFRichTextString text = new HSSFRichTextString(headers[i]);
+            cell.setCellValue(text.toString());
+        }*/
+        // 遍历集合数据，产生数据行
+        if (result != null) {
+            int index = 2;
+            for (List<String> m : result) {
+                row = sheet.createRow(index);
+                int cellIndex = 0;
+                for (String str : m) {
+                    HSSFCell cell = row.createCell((short) cellIndex);
+                    cell.setCellValue(str.toString());
+                    cellIndex++;
+                }
+                index++;
+            }
+        }
+    }
+
+    public  void download(final String path, final HttpServletResponse response) {
+        try {
+            // path是指欲下载的文件的路径。
+            final File file = new File(path);
+            // 取得文件名。
+            final String filename = file.getName();
+            // 以流的形式下载文件。
+            final InputStream fis = new BufferedInputStream(new FileInputStream(path));
+            final byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            fis.close();
+            // 清空response
+            response.reset();
+            // 设置response的Header
+            // response.addHeader("Content-Disposition", "attachment;filename="
+            // + new String(filename.getBytes()));
+            // response.addHeader("Content-Length", "" + file.length());
+            // response.setContentType("application/vnd.ms-excel;charset=utf-8");
+            final OutputStream toClient = new BufferedOutputStream(response.getOutputStream());
+
+            response.setContentType("application/x-download");// 下面三行是关键代码，处理乱码问题
+            response.setCharacterEncoding("utf-8");
+            response.setHeader("Content-Disposition",
+                    "attachment;filename=" + new String(filename.getBytes("gbk"), "iso8859-1"));
+
+            toClient.write(buffer);
+            toClient.flush();
+            toClient.close();
+        } catch (final IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
