71ddd568d31134571793e12be90c180db61c7f23
luxiaobing <luxiaobing2014@163.com>
2019-09-08 16:36:33 +0800

    测试git2

--- src/main/java/com/lxb/example/HomeController.java
+++ src/main/java/com/lxb/example/HomeController.java
@@ -201,5 +201,7 @@ public class HomeController {
     public static void main(String[] args) {
         System.out.println("hh");
         System.out.println("hh1");
+
+        System.out.println("hh2");
     }
 }
