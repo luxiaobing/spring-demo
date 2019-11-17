8da72613baa538b35e83553722d4b47aceeaa2b8
luxiaobing <luxiaobing2014@163.com>
2019-11-17 18:06:02 +0800

    test git

---
+++ src/main/java/com/lxb/test/GitTest.java
@@ -0,0 +1,13 @@
+package com.lxb.test;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author Xiaobing.Lu
+ * @create 2019-11-17 17:02
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+
+    }
+}
